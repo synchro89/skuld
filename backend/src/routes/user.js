596364a1ab11544308db10d674b143825e38501c
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-01 15:25:18 -0300

    refactor(backend): add route to user list
    add route in routes/user.js to list users

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -3,8 +3,11 @@ const routes = require('express').Router();
 const UserController = require("../controllers/users");
 
 routes.get("/", UserController.Index);
+routes.get("/:name", UserController.Get);
+
 routes.post("/", UserController.Store);
 routes.put("/:name", UserController.Update);
+
 routes.delete("/:name", UserController.Delete);
 
 module.exports = (app) => app.use("/users", routes);
