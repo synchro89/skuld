d5cf623db8673bee392293e84dd6ee87e2fda956
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-02 19:44:41 -0300

    fix(backend): fix: add auth middlware to create route
    fix: add auth middlware to create anime controller route

--- backend/src/routes/anime.js
+++ backend/src/routes/anime.js
@@ -2,6 +2,8 @@ const routes = require("express").Router();
 
 const AnimeController = require("../controllers/animes");
 
-routes.post("/", AnimeController.Create);
+const authMiddleware = require("../middlewares/auth");
+
+routes.post("/", authMiddleware(), AnimeController.Create);
 
 module.exports = app => app.use("/animes", routes);
