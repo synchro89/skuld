ef87638d3a42479b8f872cf220e9b2b4fb01a4c2
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-02 20:46:00 -0300

    feat(backend): add route to delete anime entry
    add route to delete anime entry

--- backend/src/routes/anime.js
+++ backend/src/routes/anime.js
@@ -5,5 +5,6 @@ const AnimeController = require("../controllers/animes");
 const authMiddleware = require("../middlewares/auth");
 
 routes.post("/", authMiddleware(), AnimeController.Create);
+routes.delete("/:animeId", authMiddleware(), AnimeController.Delete);
 
 module.exports = app => app.use("/animes", routes);
