f78d2c58e7506c3e13096cf1155ad2ba2abdb906
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-02 14:40:40 -0300

    feat(backend): run initial verifications
    add initial token verification to auth controlelr

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,3 +1,6 @@
+const authConfig = require("../config");
+const jwt = require("jsonwebtoken");
+
 const defaultOptions = {
     nextIfNotAuth: false
 }
@@ -8,9 +11,37 @@ const middleware = (userOptions) => {
     const { nextIfNotAuth } = options;
 
     return function (req, res, next) {
-        const { Authorization } = req.headers;
-        console.log("opa");
+        let isAuth = true;
+
+        const { Authorization: authorization } = req.headers;
+
+        if (!authorization)
+            isAuth = false;
+
+        const parts = authorization.split(" ");
+
+        if (parts.length !== 2)
+            isAuth = false;
+
+        const [scheme, token] = parts;
+
+        if (!/^Bearer$/.test(scheme))
+            isAuth = false;
+
+
+        jwt.verify(token, authConfig.secret, (error, decoded) => {
+            if (error) isAuth = false;
+
+            req.authState = {
+                isAuth,
+                userId: decoded.id
+            }
+
+            if (nextIfNotAuth) return next(authState);
+        });
     }
 }
+
+
 
 module.exports = middleware;
