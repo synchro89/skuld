dd641063dd64917bc424cdd228eba2c6afcc0af4
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-02 14:48:06 -0300

    feat(backend): add function middleware, 95% complete
    add middleware function, 95% complete

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -11,22 +11,19 @@ const middleware = (userOptions = {}) => {
     const { nextIfNotAuth } = options;
 
     return function (req, res, next) {
-        let isAuth = true;
 
         const { Authorization: authorization } = req.headers;
 
-        if (!authorization)
-            isAuth = false;
-
-        const parts = authorization.split(" ");
-
-        if (parts.length !== 2)
-            isAuth = false;
-
-        const [scheme, token] = parts;
-
-        if (!/^Bearer$/.test(scheme))
-            isAuth = false;
+        if (!nextIfNotAuth) {
+            const isAuth = isValidAuthorization(authorization);
+        } else {
+            const isAuth = isValidAuthorization(authorization)
+            req.authState = {
+                isAuth,
+                userId: isAuth ? decoded.id : null
+            }
+            next();
+        }
 
 
         jwt.verify(token, authConfig.secret, (error, decoded) => {
@@ -42,8 +39,21 @@ const middleware = (userOptions = {}) => {
     }
 }
 
-function verifyAuthorization(authorization) {
+function isValidAuthorization(authorization) {
+    if (!authorization)
+        return false;
 
+    const parts = authorization.split(" ");
+
+    if (parts.length !== 2)
+        return false;
+
+    const [scheme, token] = parts;
+
+    if (!/^Bearer$/.test(scheme))
+        return false;
+
+    return true;
 }
 
 module.exports = middleware;
