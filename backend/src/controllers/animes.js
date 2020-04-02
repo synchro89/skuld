551f1eac10bb4b48932087fe4f4b4039ed00ed1d
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-02 20:41:55 -0300

    fix(backend): add try catch to create controller of anime controller
    add try catch to create controller of anime controller

--- backend/src/controllers/animes.js
+++ backend/src/controllers/animes.js
@@ -11,47 +11,54 @@ const AnimeSchema = require("../models/AnimeSchema");
 
 const AnimeController = {
     Create: async function (req, res) {
-        let { userId, animeId } = req.body;
-        let { userId: loggedUserId } = req.authState;
+        try {
+            let { userId, animeId } = req.body;
+            let { userId: loggedUserId } = req.authState;
 
-        if (!compareId(loggedUserId, userId)) {
-            const { unauthorized } = userResponses;
+            if (!compareId(loggedUserId, userId)) {
+                const { unauthorized } = userResponses;
+                return res
+                    .status(unauthorized.status)
+                    .json(generate(unauthorized, { error: true }));
+            }
+
+            userId = stringToObjectId(userId);
+            userIdLikeString = userId.toString();
+
+            const user = await UserSchema.findOne({ _id: userId });
+
+            if (!user) {
+                const { userNotExists } = userResponses;
+                return res
+                    .status(userNotExists.status)
+                    .json(generate(userNotExists, { error: true }));
+            }
+
+            let anime = await AnimeSchema.findOne({
+                fk_user_id: userIdLikeString,
+                fk_anime_id: animeId
+            });
+
+            if (exists(anime)) {
+                const { alreadyExists } = animeResponses;
+                return res
+                    .status(alreadyExists.status)
+                    .json(generate(alreadyExists, { error: true }));
+            }
+
+            anime = await AnimeSchema.create({
+                fk_user_id: userIdLikeString,
+                fk_anime_id: animeId
+            });
+
+            const { successCreated } = animeResponses;
+            return res.json(generate(successCreated, { data: anime }));
+        } catch (error) {
+            const { unknownError } = animeResponses;
             return res
-                .status(unauthorized.status)
-                .json(generate(unauthorized, { error: true }));
+                .status(unknownError.status)
+                .json(generate(unknownError, { error }));
         }
-
-        userId = stringToObjectId(userId);
-        userIdLikeString = userId.toString();
-
-        const user = await UserSchema.findOne({ _id: userId });
-
-        if (!user) {
-            const { userNotExists } = userResponses;
-            return res
-                .status(userNotExists.status)
-                .json(generate(userNotExists, { error: true }));
-        }
-
-        let anime = await AnimeSchema.findOne({
-            fk_user_id: userIdLikeString,
-            fk_anime_id: animeId
-        });
-
-        if (exists(anime)) {
-            const { alreadyExists } = animeResponses;
-            return res
-                .status(alreadyExists.status)
-                .json(generate(alreadyExists, { error: true }));
-        }
-
-        anime = await AnimeSchema.create({
-            fk_user_id: userIdLikeString,
-            fk_anime_id: animeId
-        });
-
-        const { successCreated } = animeResponses;
-        return res.json(generate(successCreated, { data: anime }));
     },
     Delete: async function (req, res) {
         try {
