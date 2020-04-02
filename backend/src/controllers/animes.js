f4c2ec348da9d7c1884946de84465e184d8707d4
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-02 19:21:30 -0300

    feat(backend): init create controller
    init create controller

--- backend/src/controllers/animes.js
+++ backend/src/controllers/animes.js
@@ -1,6 +1,26 @@
 const { calcSkip } = require("../utils");
 
-const { anim: userResponses } = require("../responses");
+const { anime: animeResponses } = require("../responses");
+const { user: userResponses } = require("../responses");
 const { generateResponse: generate } = require("../utils");
 
+const mongoose = require('../database');
+
+const UserSchema = require("../models/UserSchema");
 const AnimeSchema = require("../models/AnimeSchema");
+
+const AnimeController = {
+    Create: async function (req, res) {
+        let { userId, animeId } = req.body;
+
+        userId = mongoose.Types.ObjectId(userId);
+
+        const user = await UserSchema.findOne({ _id: userId });
+
+        if (!user) {
+
+        }
+    }
+}
+
+module.exports = AnimeController;
