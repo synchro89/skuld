3d74d057acf7807bf86e856e1e44b7b4c2221772
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-02 19:13:42 -0300

    feat(backend): add route /animes to create anime entity
    add route /animes type post to create anime entity passing user id and anime id by req.body

--- backend/src/routes/anime.js
+++ backend/src/routes/anime.js
@@ -1,4 +1,7 @@
 const routes = require("express").Router();
 
+const AnimeController = require("../controllers/animes");
+
+routes.post("/", AnimeController.Create);
 
 module.exports = app => app.use("/animes", routes);
