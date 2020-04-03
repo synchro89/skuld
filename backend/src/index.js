2e4470622ceb458937b66ce820ace707e821a057
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-03 15:58:09 -0300

    fix(backend): remove unnecessary lib : formidable
    remove unnecessary lib : formidable

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,6 @@ require('dotenv/config');
 
 const express = require('express');
 const app = express();
-const formidable = require("express-formidable");
 const cors = require("cors");
 
 app.use(cors());
