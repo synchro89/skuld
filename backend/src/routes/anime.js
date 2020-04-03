2cef0e7f5621fb0e23867914b4132c77b08503e9
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-02 21:19:53 -0300

    feat(backend): implement route to use get controller of anime scheme
    implement route to use get controller of anime scheme

--- backend/src/routes/anime.js
+++ backend/src/routes/anime.js
@@ -5,6 +5,9 @@ const AnimeController = require("../controllers/animes");
 const authMiddleware = require("../middlewares/auth");
 
 routes.post("/", authMiddleware(), AnimeController.Create);
+
 routes.delete("/:animeId", authMiddleware(), AnimeController.Delete);
+
+routes.get("/:animeId", authMiddleware(), AnimeController.Get);
 
 module.exports = app => app.use("/animes", routes);
