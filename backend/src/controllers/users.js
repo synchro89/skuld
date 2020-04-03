e2c253ad3ba692662712f53218ffd52b03f34aff
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-02 22:00:07 -0300

    fix(backend): remove index by auth controller
    remove index by auth controller

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -16,68 +16,6 @@ const sharp = require("sharp");
 const { uploader } = require("../cloudinary");
 
 const UserController = {
-    Index: async function (req, res) {
-        try {
-            let { page, limit = 2 } = req.query;
-
-            const total = await UserSchema.find().estimatedDocumentCount();
-
-            page = Number(page);
-            limit = Number(limit);
-            totalPages = Math.ceil(total / limit);
-
-            let skip = null;
-
-
-            skip = calcSkip(page, limit).skip;
-            const currentResults = await UserSchema.find()
-                .skip(skip)
-                .limit(limit)
-                .sort([
-                    ["createdAt", "-1"]
-                ]);
-
-            if (!currentResults.length) {
-                const { notFoundPagination: notFound } = userResponses;
-                return res.status(notFound.status).json(generate(notFound, {
-                    error: true
-                }));
-            }
-
-            skip = calcSkip(page + 1, limit).skip;
-            const nextResults = await UserSchema.find()
-                .skip(skip)
-                .limit(limit)
-                .sort([
-                    ["createdAt", "-1"]
-                ]);
-
-            const metadata = {
-                hasMore: nextResults.length > 0,
-                howManyDocsInNextPage: nextResults.length,
-                totalDocs: total,
-                currentPage: page,
-                totalPages
-            }
-
-            const data = {
-                results: currentResults,
-                metadata,
-            }
-
-            const { successPagination } = userResponses;
-
-            return res.json(generate(successPagination, {
-                data
-            }));
-        } catch (error) {
-
-            const { unknownError } = userResponses;
-            return res
-                .status(unknownError.status)
-                .json(generate(unknownError, { error }));
-        }
-    },
     Get: async function (req, res) {
         try {
             let { userId, isAuth } = req.authState;
