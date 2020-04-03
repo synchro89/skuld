1177499a6b34a4c40ffaf7fe22ef1b71fabd4ccc
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-02 21:30:13 -0300

    feat(backend): when a user deleted, delete all entries of this user
    when a user deleted, delete all anime entries of this user

--- backend/src/models/preActions/user.js
+++ backend/src/models/preActions/user.js
@@ -2,6 +2,8 @@ const bcrypt = require("bcryptjs");
 
 const { uploader } = require("../../cloudinary");
 
+const AnimeSchema = require("../AnimeSchema");
+
 const preSave = model => {
     model.pre("save", async function (next) {
         const hash = await bcrypt.hash(this.password, 10);
@@ -15,6 +17,7 @@ const preDelete = model => {
     model.pre("findOneAndRemove", async function () {
         const doc = await this.findOne(this.getFilter());
         await deleteFile(doc.photo.public_id);
+        await AnimeSchema.deleteMany({ fk_user_id: doc._id.toString() });
     });
 }
 
