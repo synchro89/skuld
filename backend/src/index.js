6c43be0f976ac2709221501b3e73d3c5365fb405
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-04 14:46:12 -0300

    fix(backend): update backend files to serve in heroku
    n

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,28 +4,11 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 
+require("./database");
+
 app.use(cors());
 app.use(express.json());
 
-require("./database");
 require("./routes")(app);
 
 app.listen(process.env.PORT || 8080);
-
-// Create a tunnel to test backend
-if (process.env.DEVELOPMENT) {
-  const ngrok = require('ngrok');
-  (async function () {
-    try {
-      const url = await ngrok.connect({
-        proto: 'http',
-        addr: process.env.PORT,
-      });
-      console.log('Tunnel Created -> ');
-      console.log(url);
-      console.log('Tunnel Inspector ->  http://127.0.0.1:4040');
-    } catch (error) {
-      console.log(error);
-    }
-  })();
-}
