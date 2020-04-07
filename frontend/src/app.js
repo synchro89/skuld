cb11a1bf5e0c3c22d3e3c5366627068b9691f445
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-07 02:25:04 -0300

    fix(frontend): add params vanilla router
    add params to vanilla router

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -4,6 +4,7 @@ import Router from "./app/routes/router";
 
 document.addEventListener("DOMContentLoaded", () => {
     Router.get("/", () => { });
+    Router.get("/:id/:simdeubom", () => { });
     Router.get("*", () => { });
     Router.init();
 })
--- frontend/src/app/routes/router.js
+++ frontend/src/app/routes/router.js
@@ -20,7 +20,7 @@ const Router = {
         if (typeof userRoute === "function")
             userRoute = { render: userRoute };
 
-        const route = Object.assign({}, { path: this._publicRoute + r }, defaultRoute, userRoute);
+        const route = Object.assign({}, { path: this._publicRoute + r, params: {} }, defaultRoute, userRoute);
 
         if (r === "*") return this._404 = route;
 
@@ -29,14 +29,21 @@ const Router = {
     navigateTo: function (newRoute) {
 
     },
+    _unmontRoute: function (route) {
+
+    },
     _renderRoute: function (route) {
         console.log(route);
     },
     _setRoute: function (newRoute) {
         const matchedRoute = this._matchRoute(newRoute);
-        if (!matchedRoute) this.currentRoute = this._404;
-        else this.currentRoute = matchedRoute;
-        this._renderRoute(this.currentRoute);
+
+        if (!matchedRoute || matchedRoute.path !== this.currentRoute.path) {
+            this._unmontRoute(this.currentRoute);
+            if (!matchedRoute) this.currentRoute = this._404;
+            else this.currentRoute = matchedRoute;
+            this._renderRoute(this.currentRoute);
+        }
     },
     init: function () {
         if (!window) return;
@@ -46,6 +53,8 @@ const Router = {
         window.addEventListener("popstate", () => this._setRoute(window.location.pathname));
     },
     _matchRoute: function (route) {
+        let params = {};
+
         const [matchedRoute] = this.routes.filter(r => {
             const rPaths = r.path.divide("/");
             const routePaths = route.divide("/");
@@ -54,14 +63,21 @@ const Router = {
 
             let matched = true;
             for (let i = 0; i < rPaths.length; i++) {
-                if (!rPaths.startsWith(":")) {
-                    matched = rPaths[i] === routePaths[i];
-                }
+                const currentRPath = rPaths[i];
+                const currentRoutePath = routePaths[i];
+
+                if (!currentRPath.startsWith(":"))
+                    matched = currentRPath === currentRoutePath;
+                else
+                    params[currentRPath.replace(/[:]/g, "")] = currentRoutePath;
+
                 if (!matched) break;
             }
             return matched;
         });
-        return matchedRoute;
+
+        if (!!matchedRoute) return Object.assign({}, matchedRoute, { params });
+        else return null;
     }
 }
 
