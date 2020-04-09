ed076721b6dcf16a8ff802c8e7a1ab9720196ccb
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-09 20:18:12 -0300

    feat(front): add button to show/hide password
    add button to show/hide password

--- frontend/src/views/pages/login/index.js
+++ frontend/src/views/pages/login/index.js
@@ -21,20 +21,30 @@ export default function LoginPage() {
             <canvas id="auth-canvas"></canvas>
             <div class="auth-wrapper">
                 <form class="auth-form" autocomplete="off">
-
-                    <label class="auth-wrapper__label auth-wrapper__label--as-wrapper" for="login_name">USERNAME</label>
-                    <div class="auth-input-wrapper">
-                        <span class="auth-input-wrapper__icon material-icons">person</span>
-                        <input class="auth-input-wrapper__input" id="login_name" type="text" name="login_name"
-                            autofocus>
+                    
+                    <header class="auth-form__header">
+                        <h1>Welcome Back!</h1>
+                        <p>We are glad for see you again!</p>
+                    </header>
+                    <div class="auth-wrapper__field">
+                        <label class="auth-wrapper__label auth-wrapper__label--as-wrapper" for="login_name">USERNAME</label>
+                        <div class="auth-input-wrapper">
+                            <span class="auth-input-wrapper__icon material-icons">person</span>
+                            <input class="auth-input-wrapper__input" id="login_name" type="text" name="login_name"
+                                autofocus>
+                        </div>
                     </div>
 
-                    <label class="auth-wrapper__label auth-wrapper__label--as-wrapper" for="login_name">PASSWORD</label>
-                    <div class="auth-input-wrapper">
-                        <span class="auth-input-wrapper__icon material-icons">https</span>
-                        <input class="auth-input-wrapper__input" id="login_password" type="password" name="login_password">
+                    <div class="auth-wrapper__field">
+                        <label class="auth-wrapper__label auth-wrapper__label--as-wrapper" for="login_password">PASSWORD</label>
+                        <div class="auth-input-wrapper">
+                            <span class="auth-input-wrapper__icon material-icons">https</span>
+                            <input class="auth-input-wrapper__input" id="login_password" type="password" name="login_password">
+                            <span id="show_password" data-target="login_password" class="pointer auth-input-wrapper__icon material-icons">visibility</span>
+                        </div>
+                        <a class="auth-wrapper__label auth-wrapper__label--active border-on-hover use-padding" href="/reset">Reset password</a>
                     </div>
-                    <a class="auth-wrapper__label auth-wrapper__label--active" href="/reset">Reset password</a>
+                    
 
                     <button class="auth-submit" type="submit">Login</button>
                     <p
@@ -59,6 +69,16 @@ export default function LoginPage() {
             const form = document.getElementsByClassName("auth-wrapper")[0];
             const buttonSubmit = document.getElementsByClassName("auth-submit")[0];
             const links = document.querySelectorAll(".auth-wrapper a");
+
+            const showPass = document.getElementById("show_password");
+            const inputTarget = document.getElementById(showPass.getAttribute("data-target"));
+
+            showPass.onclick = function () {
+                const newType = inputTarget.getAttribute("type") === "password" ? "text" : "password";
+                const newIcon = newType === "password" ? "visibility" : "visibility_off"
+                inputTarget.setAttribute("type", newType);
+                showPass.textContent = newIcon;
+            }
 
             for (const link of links) {
                 link.onclick = e => {
