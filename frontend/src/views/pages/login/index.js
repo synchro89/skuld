49babced6331fe3ae1018b2e228c01e64ed086ae
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-09 19:21:27 -0300

    feat(frontend): add button to register page
    add button to register page

--- frontend/src/views/pages/login/index.js
+++ frontend/src/views/pages/login/index.js
@@ -22,22 +22,32 @@ export default function LoginPage() {
             <div class="auth-wrapper">
                 <form class="auth-form" autocomplete="off">
 
-                    <label class="auth-wrapper__label" for="login_name">USERNAME</label>
+                    <label class="auth-wrapper__label auth-wrapper__label--as-wrapper" for="login_name">USERNAME</label>
                     <div class="auth-input-wrapper">
                         <span class="auth-input-wrapper__icon material-icons">person</span>
                         <input class="auth-input-wrapper__input" id="login_name" type="text" name="login_name"
-                            placeholder="Username" autofocus>
+                            autofocus>
                     </div>
 
-                    <label class="auth-wrapper__label" for="login_name">PASSWORD</label>
+                    <label class="auth-wrapper__label auth-wrapper__label--as-wrapper" for="login_name">PASSWORD</label>
                     <div class="auth-input-wrapper">
                         <span class="auth-input-wrapper__icon material-icons">https</span>
-                        <input class="auth-input-wrapper__input" id="login_password" type="password" name="login_password" placeholder="Password">
+                        <input class="auth-input-wrapper__input" id="login_password" type="password" name="login_password">
                     </div>
-                    <a href="/register" class="auth-link">Need a account? Register</a>
+                    <a class="auth-wrapper__label auth-wrapper__label--active" href="/reset">Reset password</a>
 
                     <button class="auth-submit" type="submit">Login</button>
-
+                    <p
+                        class="auth-wrapper__label auth-wrapper__label--disabled"
+                    >
+                        Need a account? 
+                        <a 
+                            href="/register" 
+                            class="auth-wrapper__label--active border-on-hover"
+                        >
+                            Register
+                        <a>
+                    </p>
                 </form>
             </div>
             `
@@ -48,15 +58,18 @@ export default function LoginPage() {
         didRender: async function (props) {
             const form = document.getElementsByClassName("auth-wrapper")[0];
             const buttonSubmit = document.getElementsByClassName("auth-submit")[0];
-            const authLink = document.getElementsByClassName("auth-link")[0];
+            const links = document.querySelectorAll(".auth-wrapper a");
+
+            for (const link of links) {
+                link.onclick = e => {
+                    e.preventDefault();
+                    Router.navigateTo(link.href);
+                }
+            }
 
             form.onsubmit = e => {
                 e.preventDefault();
                 console.log(e);
-            }
-            authLink.onclick = e => {
-                e.preventDefault();
-                Router.navigateTo(authLink.href);
             }
 
             const removeRipple = new Ripple(buttonSubmit, {
@@ -65,7 +78,6 @@ export default function LoginPage() {
             });
 
             canvas(document.getElementById("auth-canvas"));
-
 
             return {
                 removeRipple
